e6508f0ee8f7c5e65659b721c3dabe43e45c7d5c
heitorcallipo <heitorcallipo14@gmail.com>
2023-03-15 14:48:20 -0300

    Update commit

---
+++ Landing Page/script.js
@@ -0,0 +1,5 @@
+const btnTP = document.querySelector(".btnTP");
+const btnMP = document.querySelector("btnMP");
+const btnTHP = document.querySelector("btnTHP");
+const btnFP = document.querySelector("btnFP");
+const btnFiP = document.querySelector("btnFiP");
